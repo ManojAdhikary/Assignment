6ab42c6d89a6b7a450a0032171ab40965f6e4ddc
Manoj Adhikari <manojadqa@gmail.com>
2022-04-14 22:32:10 -0500

    adding scnaner as assignment 1

---
+++ src/main/java/Assignment/PizzaScanner.java
@@ -0,0 +1,28 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class PizzaScanner {
+	static int price;
+	static int quantity;
+	static int discount;
+	static int finalPrice;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the price of each PIZZA : ");
+		price = sc.nextInt();
+
+		System.out.println("Enter the quanity of PIZZA : ");
+		quantity = sc.nextInt();
+
+		System.out.println("Enter the discount of pizza : ");
+		discount = sc.nextInt();
+
+		finalPrice = (price * quantity - (price * quantity * discount / 100));
+		System.out.println("The final price of pizza after discount is : " + finalPrice);
+
+	}
+
+}
